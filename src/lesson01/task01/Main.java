d28a13d30bfed9aaabe772bbb243f3d6932af6d2
evgenboldyr <sapegboldyr@gmail.com>
2016-12-17 17:55:08 +0200

    Commit task01 classes main fix

--- src/lesson01/task01/Main.java
+++ src/lesson01/task01/Main.java
@@ -1,11 +1,13 @@
 package lesson01.task01;
 
+import java.io.IOException;
+
 /**
  * Created by IEvgen Boldyr on 17.12.16.
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         PersonsConsole console = new PersonsConsole();
         console.start();
     }
