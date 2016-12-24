09e1b960da8335a9a74ba65bf52d49f445835412
Bohdan Koreniak <imrahill120@gmail.com>
2016-12-24 17:08:01 +0200

    homework week 1

---
+++ BohdanKoreniak/src/hw1/lesson1/task2/Main.java
@@ -0,0 +1,14 @@
+package hw1.lesson1.task2;
+
+import hw1.lesson1.task2.managers.PersonManager;
+
+/**
+ * Created by Bohdan on 17.12.2016.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        new PersonManager().run();
+    }
+}
