1f76fdbb7f4705049aa6840a367457dd10875399
eriol4ik <eriol4ik@gmail.com>
2016-12-24 22:00:28 +0200

    Homework 02: binary tree

---
+++ VadymKovalchuk/src/hw02/TreeMapVsOther.java
@@ -0,0 +1,45 @@
+package lesson02.ex02;
+
+import hw02.TreeMap;
+
+import java.util.ArrayList;
+
+/**
+ * Created by eriol4ik on 20/12/2016.
+ */
+public class TreeMapVsOther {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        TreeMap<Integer, String> treeMap = new TreeMap<>();
+        java.util.TreeMap<Integer, String> treeMapUtil = new java.util.TreeMap<>();
+
+        System.out.println("Добавление элементов:");
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 10_000; i++) {
+            arrayList.add("a");
+        }
+        long end = System.currentTimeMillis();
+        long arrayListTime = end - start;
+
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 10_000; i++) {
+            treeMap.add(i, Integer.toString((int)(Math.random() * 1_000_000)));
+//            treeMap.add((int)(Math.random() * 10_000), "a");
+        }
+        end = System.currentTimeMillis();
+        long treeMapTime = end - start;
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 10_000; i++) {
+            treeMap.add(i, Integer.toString((int)(Math.random() * 1_000_000)));
+//            treeMapUtil.put((int)(Math.random() * 10_000), "a");
+        }
+        end = System.currentTimeMillis();
+        long treeMapUtilTime = end - start;
+
+        System.out.println(arrayListTime);
+        System.out.println(treeMapTime);
+        System.out.println(treeMapUtilTime);
+    }
+}
