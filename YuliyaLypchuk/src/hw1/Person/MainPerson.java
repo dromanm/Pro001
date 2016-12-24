264e909d508833219580ae69599ccf8e72fcb111
JuliyaLypchuk <yuliya.lypchuk@gmail.com>
2016-12-24 17:44:33 +0200

    hw1.Person

---
+++ YuliyaLypchuk/src/hw1/Person/MainPerson.java
@@ -0,0 +1,13 @@
+package hw1.Person;
+
+import java.io.IOException;
+
+/**
+ * Created by JL on 17.12.2016.
+ */
+public class MainPerson {
+    public static void main(String[] args) throws IOException {
+        PersonOptions person = new PersonOptions();
+        person.start();
+    }
+}
