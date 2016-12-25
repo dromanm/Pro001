7953a221325913c3f572496b4ca12328437bd169
AleksandrVyazovsky <san110489@gmail.com>
2016-12-25 16:27:32 +0200

    first

--- AleksandrVyazovsky/src/Test123.java
+++ AleksandrVyazovsky/src/Test123.java
@@ -1,5 +1,10 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by Олег on 24.12.2016.
  */
 public class Test123 {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
 }
