ded81299a1c70ee89ef041382e5dc64760ecda77
Bohdan <imrahill120@gmail.com>
2017-01-01 21:24:49 +0200

    Just another commit with week #1 tasks due to previous commit error

---
+++ BohdanKoreniak/src/w1/lesson1/task1/Main.java
@@ -0,0 +1,20 @@
+package w1.lesson1.task1;
+
+/**
+ * Created by Bohdan on 17.12.2016.
+ */
+public class Main
+{
+    public static void main(String[] args)
+    {
+        User user = new User("user1", "user1", 2016);
+        User user1 = new User("user1", "user1", 2015);
+        User user2 = new User("user1", "user1", 2016);
+
+        System.out.println(user.equals(user1));
+        System.out.println(user.equals(user2));
+
+        System.out.println(user.hashCode() == user1.hashCode());
+        System.out.println(user.hashCode() == user2.hashCode());
+    }
+}
---
+++ BohdanKoreniak/src/w1/lesson1/task1/User.java
@@ -0,0 +1,55 @@
+package w1.lesson1.task1;
+
+/**
+ * Created by Bohdan on 17.12.2016.
+ */
+public class User
+{
+    private String login;
+    private String password;
+    private int year;
+
+    public User(String login, String password, int year)
+    {
+        this.login = login;
+        this.password = password;
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object object)
+    {
+        if (this == object)
+        {
+            return true;
+        }
+        if (object != null && getClass().equals(object.getClass()))
+        {
+            User user = (User) object;
+
+            if (!login.equals(user.login))
+            {
+                return false;
+            }
+
+            if (!password.equals(user.password))
+            {
+                return false;
+            }
+
+            if (year != user.year)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return login.hashCode() * password.hashCode() * year * 31;
+    }
+}
