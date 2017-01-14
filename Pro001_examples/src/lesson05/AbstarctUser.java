7de26d4bc9c43a2c775668980e1b10f99b1c3bb5
evgenboldyr <sapegboldyr@gmail.com>
2017-01-14 17:37:49 +0200

    Reflection Part2

--- Pro001_examples/src/lesson05/AbstarctUser.java
+++ Pro001_examples/src/lesson05/AbstarctUser.java
@@ -1,7 +1,9 @@
 package lesson05;
 
+import java.io.Serializable;
+
 /**
  * Created by IEvgen Boldyr on 14.01.17.
  */
-public class AbstarctUser {
+public class AbstarctUser implements Serializable {
 }
