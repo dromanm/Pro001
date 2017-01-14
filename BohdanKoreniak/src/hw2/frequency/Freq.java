2754eba8f893398321aeabff830241bffb18abd3
Bohdan <imrahill120@gmail.com>
2017-01-14 13:21:26 +0200

    Frequency task

--- BohdanKoreniak/src/hw2/frequency/Freq.java
+++ BohdanKoreniak/src/hw2/frequency/Freq.java
@@ -3,7 +3,9 @@ package hw2.frequency;
 import hw2.authentication.ConsoleManager;
 
 import java.io.IOException;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Bohdan on 12.01.2017.
@@ -11,6 +13,8 @@ import java.util.Random;
 public class Freq
 {
     private String text;
+
+    private Map<String, Long> mapWordsByFrequency = new HashMap<>();
 
     public String setTextFromConsole()
     {
@@ -36,5 +40,109 @@ public class Freq
         }
 
         return text = stringBuilder.toString();
+    }
+
+    private void collectWordsByFrequency()
+    {
+        if (!mapWordsByFrequency.isEmpty())
+        {
+            return;
+        }
+
+        Stream<String> stream = Stream.of(text.toLowerCase().split("[^\\d\\w']+"));
+
+        mapWordsByFrequency = stream.collect(Collectors.groupingBy(String::toString, Collectors.counting()));
+    }
+
+    public Set<String> getWordsByFrequency(int frequency)
+    {
+        collectWordsByFrequency();
+
+        Set<String> words = new HashSet<>();
+
+        mapWordsByFrequency.forEach((key, value) ->
+        {
+            if (value.intValue() == frequency)
+            {
+                words.add(key);
+            }
+        });
+
+        return words;
+    }
+
+    public Set<String> getWordsByFrequencyLessThan(int frequency)
+    {
+        collectWordsByFrequency();
+
+        Set<String> words = new HashSet<>();
+
+        mapWordsByFrequency.forEach((key, value) ->
+        {
+            if (value.intValue() < frequency)
+            {
+                words.add(key);
+            }
+        });
+
+        return words;
+    }
+
+    public Set<String> getWordsByFrequencyMoreThan(int frequency)
+    {
+        collectWordsByFrequency();
+
+        Set<String> words = new HashSet<>();
+
+        mapWordsByFrequency.forEach((key, value) ->
+        {
+            if (value.intValue() > frequency)
+            {
+                words.add(key);
+            }
+        });
+
+        return words;
+    }
+
+    public void printAcs()
+    {
+        collectWordsByFrequency();
+
+        mapWordsByFrequency.entrySet().stream().sorted(Map.Entry.comparingByValue()).forEach(entry ->
+        {
+            ConsoleManager.printText(entry.getKey() + "\t" + entry.getValue());
+        });
+    }
+
+    public void printDesc()
+    {
+        collectWordsByFrequency();
+
+        mapWordsByFrequency.entrySet().stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed()).forEach(entry ->
+        {
+            ConsoleManager.printText(entry.getKey() + "\t" + entry.getValue());
+        });
+    }
+
+    public static void main(String[] args)
+    {
+        Freq freq = new Freq();
+
+        try
+        {
+            ConsoleManager.printText(freq.setTextFromFile("src/hw2/frequency/lyrics.txt"));
+            ConsoleManager.printText("");
+
+            freq.printAcs();
+            ConsoleManager.printText("");
+
+            freq.printDesc();
+            ConsoleManager.printText("");
+        }
+        catch (IOException e)
+        {
+            ConsoleManager.printText(e.getMessage());
+        }
     }
 }
