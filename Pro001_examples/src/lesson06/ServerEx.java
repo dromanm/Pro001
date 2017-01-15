80f88c948f9fda9720b43d13269a3169ee2df6d5
evgenboldyr <sapegboldyr@gmail.com>
2017-01-15 16:13:30 +0200

    LAN Examples Part1

---
+++ Pro001_examples/src/lesson06/ServerEx.java
@@ -0,0 +1,29 @@
+package lesson06;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+
+/**
+ * Created by IEvgen Boldyr on 15.01.17.
+ */
+public class ServerEx {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel channel = ServerSocketChannel.open();
+        channel.bind(new InetSocketAddress(8089));
+
+        SocketChannel socket = channel.accept();
+
+        ByteBuffer buffer = ByteBuffer.allocate(128);
+        int bytes = 0;
+        while ((bytes = socket.read(buffer)) > 0) {
+            buffer.flip();
+            String message = new String(buffer.array(), 0, bytes);
+            System.out.println(message);
+        }
+    }
+}
