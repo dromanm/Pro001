f322ade1713eb7dca7378313e3f8ff52ac23d5e8
evgenboldyr <sapegboldyr@gmail.com>
2017-02-11 16:53:30 +0200

    HIbernate @OneToMany - @ManyToOne

---
+++ Pro001_examples/src/hiberannotations/hibernate_onetomany/OneToManyEx.java
@@ -0,0 +1,32 @@
+package hiberannotations.hibernate_onetomany;
+
+import hiberannotations.hibernate_onetomany.domain.Car;
+import hiberannotations.hibernate_onetomany.domain.Driver;
+import hiberannotations.hibernate_onetomany.util.HibernateUtil;
+import org.hibernate.Session;
+
+/**
+ * Created by IEvgen Boldyr on 11.02.17.
+ */
+
+public class OneToManyEx {
+
+    public static void main(String[] args) {
+        Car car = new Car("TEST", "TEST");
+
+        Session session =
+                HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.saveOrUpdate(car);
+        session.getTransaction().commit();
+
+        Driver driver1 = new Driver("TEST1", "TEST2", car);
+        Driver driver2 = new Driver("TEST2", "TEST2", car);
+
+        session.beginTransaction();
+        session.saveOrUpdate(driver1);
+        session.saveOrUpdate(driver2);
+        session.getTransaction().commit();
+    }
+
+}
---
+++ Pro001_examples/src/hiberannotations/hibernate_onetomany/domain/Driver.java
@@ -0,0 +1,68 @@
+package hiberannotations.hibernate_onetomany.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by IEvgen Boldyr on 11.02.17.
+ */
+
+@Entity
+@Table(name = "DRIVERS")
+public class Driver {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "driver_seq")
+    @SequenceGenerator(name = "driver_seq", sequenceName = "driver_id", allocationSize = 10)
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "SURNAME")
+    private String surname;
+
+    @ManyToOne
+    private Car car;
+
+    public Driver() {}
+
+    public Driver(String name, String surname, Car car) {
+        this.name = name;
+        this.surname = surname;
+        this.car = car;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
+
+
